9be74a3ab46bbb62a0861f43f46dfcdbd1482dcd
Kelly Markiewicz <kellypoon@gmail.com>
2016-08-13 11:26:02 -0600

    Commit first half of passing udacidata_test

--- data/seeds.rb
+++ data/seeds.rb
@@ -5,8 +5,9 @@ require 'faker'
 
 def db_seed
   # Use the faker gem to generate arrays with fake brands, 
-# product names, and prices.
-
-# Create 10 new Product objects, and save them to the 
-# database
+	# product names, and prices.
+	# Create 10 new Product objects, and save them to the database
+	10.times do
+    	Product.create(brand: Faker::Company.name, name: Faker::Commerce.product_name, price: Faker::Commerce.price)
+    end
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -6,63 +6,70 @@ class Udacidata
 	# include UdacidataErrors
 	# raise UdacidataErrors::ProductNotFoundError
 
-#   Product.all should return an array of Product objects representing all 
-#   the data in the database.
-#   Product.all
-# #=> [#<Product:0x007fe4853e06e0 @id=0, @brand="Lego", @name="Sleek Wool Bottle", @price="81.87">, 
-# #<Product:0x007fe4853e0640 @id=1, @brand="Hasbro", @name="Sleek Wool Bottle", @price="81.54">, 
-# #<Product:0x007fe4853e05a0 @id=2, @brand="Crayola", @name="Durable Concrete Bag", @price="45.35">, 
-# ...]
-  def all
-  end
+	@@data_path = File.dirname(__FILE__) + "/../data/data.csv"
 
-def self.create(attributes = nil)
-  # If the object's data is already in the database
-  # create the object
-  # return the object
+	def self.create(attributes = nil)
+  		# If the object's data is already in the database
+  		# create the object
+  		# return the object
+  		# id is the variable that determines if product already exists
+  		# The attributes represent an object that already exists in the database. Pass 
+		# the attributes into new, and return the created object.
+  		product = Product.new({id: attributes[:id], brand: attributes[:brand], 
+  			name: attributes[:name], price: attributes[:price]})	
 
-  # If the object's data is not in the database
-  # create the object
-  # save the data in the database
-  # return the object
+  		# If the object's data is not in the database
+  		# create the object
+  		# save the data in the database
+  		# return the object
+  		# The attributes passed in represent an object that does not yet exist in the 
+		# database. Pass the attributes into new and save the object data to the database.
 
-  # Product.create should create an instance of the object and save the data to 
-  # the database. It will return the Product object it created.
-  # Product.create(brand: "Udacity", name: "yoyo", price: 5.00)
-  # #=> #<Product:0x007f8b5a99aee8>
+		CSV.open(@@data_path, "a") do |csv|
+  			csv << [product.id, product.brand, product.name, product.price]
+  		end	
 
-#   The attributes represent an object that already exists in the database. Pass the attributes into 
-#   new, and return the created object.
-
-# The attributes passed in represent an object that does not yet exist in the database. Pass 
-# the attributes into new and save the object data to the database.
-end 
+  		return product
+	end 
 
 
+	#   Product.all should return an array of Product objects representing all 
+	#   the data in the database.
+	#   Product.all
+  	def self.all
+  		products = [] 
+    	CSV.read(@@data_path, headers:true).each do |product| 
+      		products << Product.new(id: product["id"], brand: product["brand"], 
+      			name: product["product"], price: product["price"])
+    	end
+    	return products
+  	end
 
-  # Product.first should return a Product object that represents the 
-  # first product in the database.
-  # Product.first
-  #=> #<Product:0x007fdf90ba9e20>
-  def first
-  end
+  	# Product.first should return a Product object that represents the 
+  	# first product in the database.
+  	# Product.first
+  	#=> #<Product:0x007fdf90ba9e20>
+  	# Product.first(2)
+	#=> [#<Product:0x007f8a91a2c9c0 @id=0, @brand="Fisher-Price", @name="Gorgeous Plastic Bag", @price="44.99">
+  	def self.first(n = 1)
+  		if n==1
+			self.all.first(1)[0]
+		else
+			self.all.first(n)
+		end	
+  	end
 
-# Product.first(n) will return an array of Product objects for the 
-# first n products in the database.
-# Product.first(2)
-#=> [#<Product:0x007f8a91a2c9c0 @id=0, @brand="Fisher-Price", @name="Gorgeous Plastic Bag", @price="44.99">, 
-#<Product:0x007f8a91a2c920 @id=1, @brand="Nintendo", @name="Lightweight Iron Clock", @price="37.36">]
-  def first(n)
-  end
-
-
-
-  # Product.last should return a Product object that represents the last 
-  # product in the database. 
-  # Product.last
-  # #=> #<Product:0x007f807da70508>
-  def last
-  end
+  	# Product.last should return a Product object that represents the last 
+  	# product in the database. 
+  	# Product.last
+  	# #=> #<Product:0x007f807da70508>
+  	def self.last(n = 1)
+  		if n==1
+			self.all.last(1)[0]
+		else
+			self.all.last(n)
+		end	
+  	end
 
 #   Product.last(n) will return an array of Product objects for the last n 
 #   products in the database.
@@ -137,6 +144,8 @@ end
 # # Get product with id 5, change its brand to "Udacity"
 # Product.find(5).update(brand: "Udacity")
 # #=> #<Product:0x007fbff1a44558>
-	def product_instance.update
-	end
+	# def product_instance.update
+	# end
 end
+
+
